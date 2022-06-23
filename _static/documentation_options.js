c9115ed0825b1ccf817e619d33c810d9b5e427c5
JohnVidler <j.vidler@lancaster.ac.uk>
2022-06-23 12:02:27 +0000

    Deploying to gh-pages from @ lancaster-university/codal-documentation@074263aff246b038b1296853813e27edb51af17a 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'codal-microbit-v2 v0.2.40',
+    VERSION: 'v0.2.40',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
