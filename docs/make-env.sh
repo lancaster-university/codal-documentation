8a83d9f07c2064352a80458493d8ff922de938f3
John Vidler <j.vidler@lancaster.ac.uk>
2022-07-03 23:18:20 +0100

    Added new ignore rules, a basic requirements.txt and a virtualenv make-env script

---
+++ docs/make-env.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+python3 -m venv env
+source env/bin/activate
+
+pip install -r requirements.txt
+
+pip uninstall breathe
+
+mkdir lib
+git clone https://github.com/JohnVidler/breathe.git lib/breathe
+cd lib/breathe
+python3 setup.py build
+python3 setup.py install
+cd ../../
+
+make html
+
+deactivate
