7d8219dcd8f8222a04dcc7752b580eaf1a62a276
John Vidler <j.vidler@lancaster.ac.uk>
2022-07-04 11:08:55 +0100

    Reversing the install order to try and force pip to behave and use the local copy in the venv...

--- docs/make-env.sh
+++ docs/make-env.sh
@@ -8,10 +8,6 @@ source env/bin/activate
 
 python3 -m pip install --upgrade pip
 
-echo "!!! INSTALLING REQUIREMENTS !!!"
-#pip install -r requirements.txt
-pip install Sphinx exhale sphinx-book-theme
-
 echo "!!! BUILDING NEW BREATHE !!!"
 mkdir lib
 git clone https://github.com/JohnVidler/breathe.git lib/breathe
@@ -19,6 +15,10 @@ cd lib/breathe
 python3 setup.py build
 python3 setup.py install
 cd ../../
+
+echo "!!! INSTALLING REQUIREMENTS !!!"
+#pip install -r requirements.txt
+pip install Sphinx exhale sphinx-book-theme
 
 echo "!!! BUILDING DOCUMENTATION !!!"
 make html
