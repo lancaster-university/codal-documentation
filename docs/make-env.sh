57720b88e881988f975f80d2d5fedef6e60aaaa5
John Vidler <j.vidler@lancaster.ac.uk>
2022-07-04 02:26:52 +0100

    Added section headers to debug

--- docs/make-env.sh
+++ docs/make-env.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+echo "!!! MOVING TO VIRTUAL ENV !!!"
 python3 -m venv env
 source env/bin/activate
 
+echo "!!! INSTALLING REQUIREMENTS !!!"
 pip install -r requirements.txt
 
 pip uninstall -y breathe
 
+echo "!!! BUILDING NEW BREATHE !!!"
 mkdir lib
 git clone https://github.com/JohnVidler/breathe.git lib/breathe
 cd lib/breathe
@@ -14,6 +17,8 @@ python3 setup.py build
 python3 setup.py install
 cd ../../
 
+echo "!!! BUILDING DOCUMENTATION !!!"
 make html
 
+echo "!!! EXITING VIRTUAL ENV !!!"
 deactivate
